8a3c155d04bc1087c4fe4ccb6e900b51a103f04b
sheiksaga <gandhi.sangeeth@gmail.com>
2023-03-28 08:58:00 +0200

    top button and design fixes 	modified:   assets/index.css 	modified:   assets/main.css 	modified:   assets/main.js 	modified:   assets/posts.css 	modified:   index.html 	modified:   posts/2023/Website-builds.html 	modified:   posts/2023/what-is-this.html 	modified:   posts/list.html

--- assets/main.js
+++ assets/main.js
@@ -48,3 +48,20 @@ $('#theme-toggle-btn').click(function() {
     $('body').toggleClass('dark-theme');
   });
 
+//back to top
+$(document).ready(function(){
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > $(this).height() * 0.4) {
+            $('#back-to-top').fadeIn();
+        } else {
+            $('#back-to-top').fadeOut();
+        }
+    });
+
+    $('#back-to-top').click(function () {
+        $("html, body").animate({
+            scrollTop: 0
+        }, 600);
+        return false;
+    });
+});
